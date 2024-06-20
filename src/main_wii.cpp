14a0f7bb314de9ef8cd652769224d3591b87baf1
radicalten <79894181+radicalten@users.noreply.github.com>
2024-06-20 09:01:58 -0500

    Update main_wii.cpp

--- src/main_wii.cpp
+++ src/main_wii.cpp
@@ -44,13 +44,11 @@ int main() {
 	VIDEO_WaitVSync();
 	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
-
 	// The console understands VT terminal escape codes
 	// This positions the cursor on row 2, column 0
 	// we can use variables for this with format codes too
 	// e.g. printf ("\x1b[%d;%dH", row, column );
 	printf("\x1b[2;0H");
-
 
 	printf("Hello World!\n");
 
