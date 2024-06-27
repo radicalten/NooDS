dd612b4436e71720dd4d3536a6591803503ee68b
radicalten <79894181+radicalten@users.noreply.github.com>
2024-06-27 08:41:43 -0500

    Update main_wii.cpp

--- src/main_wii.cpp
+++ src/main_wii.cpp
@@ -74,7 +74,7 @@ int LoadGLTextures( )
     /* Create storage space for the texture */
     SDL_Surface *TextureImage[1]; 
 
-    /* Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit */
+*/ Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit 
     if ( ( TextureImage[0] = SDL_LoadBMP_RW(SDL_RWFromConstMem(crate_tex,size_crate_tex),1) ) )
         {
 
@@ -130,9 +130,12 @@ int LoadGLTextures( )
 			       GL_UNSIGNED_BYTE, TextureImage[0]->pixels );
         }
 		else
+
 		{
 			printf("Could not load the texture\n");
 		}
+
+*/
 
     /* Free up any memory we may have used */
     if ( TextureImage[0] )
