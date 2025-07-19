c8380f5000ef39d0dcb4a7a9aefae086e40bd833
radicalten <79894181+radicalten@users.noreply.github.com>
2025-07-19 04:05:41 -0500

    Update main.c

--- wii-examples/source2/main.c
+++ wii-examples/source2/main.c
@@ -32,7 +32,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #define _GNU_SOURCE
 
-#include <GL/glu.h>
+#include </opt/devkitpro/portlibs/wii/include/GL/glu.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <math.h>
